41bd963a3bb1a15be677c944718f8364d99ec46c
Rohankhan999 <rohankhan08453@gmail.com>
2024-09-19 01:20:38 +0500

    Update app.js

--- app.js
+++ app.js
@@ -46,3 +46,63 @@ console.log(arr[2].join(" ") );
 // document.write( "Element at Index 1 is " +arry[1] +"<br>");
 // document.write( "Element at Index 2 is " +arry[2] +"<br>");
 // document.write("Element at Index 3 is " +arry[3] +"<br>");
+
+//Question no 06//
+
+// var counting = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+// var counting = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+// var even = [0,2,4,6,8,10,12,14,16,18,20];
+// var odd  = [1,3,5,7,9,11,13,15,17,19,21];
+// var series = ["2k","4k","6k","8k","10k","12k","14k","16k","18k","20k"];
+
+
+// document.write("<h1>" +"Counting :  <br> <br>   " + counting  + "</h1>");
+// document.write("<h1>" +"Reverse Counting :  <br> <br>   " + counting.reverse("")  + "</h1>");
+// document.write("<h1>" +"Even :  <br> <br>   " + even  + "</h1>");
+// document.write("<h1>" +"Odd :  <br> <br>   " + odd  + "</h1>");
+// document.write("<h1>" +"Series :  <br> <br>   " + series  + "</h1>");
+
+//Question no 07//
+
+// var bakeryitems = ["bread","Rusk","Cookie","Cake","Chips","Patties"];
+// var userinput = prompt("Enter which Item you Want!")
+
+// if(userinput === bakeryitems[0]){
+//     alert(bakeryitems[0] + " this item is avialable in our bakery at 0 index!")
+// }
+// else if(userinput === bakeryitems[1]){
+//     alert(bakeryitems[1] + " this item is avialable in our bakery at 1 index!")
+// }
+// else if(userinput === bakeryitems[2]){
+//     alert(bakeryitems[2] + " this item is avialable in our bakery at 2 index!")
+// }
+// else if(userinput === bakeryitems[3]){
+//     alert(bakeryitems[3] + " this item is avialable in our bakery at 3 index!")
+// }
+// else if(userinput === bakeryitems[4]){
+//     alert(bakeryitems[4] + " this item is avialable in our bakery at 4 index!")
+// }
+// else if(userinput === bakeryitems[5]){
+//     alert(bakeryitems[5] + " this item is avialable in our bakery at 5 index!")
+// }
+// else{
+//     alert("This Item is not avialable!")
+// };
+
+//Question no 08//
+
+// var A = [24, 53, 78, 91, 12] 
+// document.write("<h1>" + "Array Items = " + A +"</h1>")
+// document.write("<h1>" + "Largest number = " + A[3] +"</h1>")
+
+//Question no 09//
+
+// var A = [24, 53, 78, 91, 12] 
+// document.write("<h1>" + "Array Items = " + A +"</h1>")
+// document.write("<h1>" + "Smallest number = " + A[4] +"</h1>")
+
+//Question no 10//
+
+// for(var i = 1 ; i <= 20 ; i++){
+//     document.write("<p>"+i*5);
+// }
